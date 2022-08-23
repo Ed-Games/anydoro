8f9d7e3dd8d5947d38d902ca7edb0938e7d2eec2
Ed-Games <edmarcosfilho2015@gmail.com>
2022-08-22 22:27:28 -0300

    update: wrapped room component with the context provider for the timer

--- src/pages/room/[slug].tsx
+++ src/pages/room/[slug].tsx
@@ -2,16 +2,19 @@ import { NextPage } from "next";
 import Head from "next/head";
 import Header from "../../components/Header";
 import Pomodoro from "../../components/Pomodoro";
+import { TimerContextProvider } from "../../contexts/timerContext";
 
 const Room: NextPage = () => {
   return (
-    <div className="container">
-      <Head>
-        <title>Anydoro | Timer </title>
-      </Head>
-      <Header />
-      <Pomodoro />
-    </div>
+    <TimerContextProvider>
+      <div className="container">
+        <Head>
+          <title>Anydoro | Timer </title>
+        </Head>
+        <Header />
+        <Pomodoro />
+      </div>
+    </TimerContextProvider>
   );
 };
 
