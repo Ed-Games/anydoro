1a03af3470b164241c172d1edcec8505470b7c7b
Ed-Games <edmarcosfilho2015@gmail.com>
2023-01-24 09:48:56 -0300

    create: Timer Input component

---
+++ src/components/TimeInput/index.tsx
@@ -0,0 +1,39 @@
+import { FiClock } from "react-icons/fi";
+import MaskedInput from "react-text-mask";
+
+import styles from './styles.module.scss';
+
+const timeRegex = [/[0-5]/, /[0-9]/, ":", /[0-5]/, /[0-9]/];
+
+interface ITimeInputProps {
+  name: string;
+  title: string;
+  value: string;
+  error?: string;
+  showError?: boolean;
+  onChange: (...args:any[]) => void;
+}
+
+export const TimeInput = ({ title, name, value, error, showError, onChange  }: ITimeInputProps ) => {
+  return (
+    <div className="column-wrapper">
+      <span>{title}</span>
+      <div className={`row-wrapper ${styles.timeInput}`}>
+        <MaskedInput
+          mask={timeRegex}
+          placeholder="00:00"
+          value={value}
+          name={name}
+          onChange={onChange}
+          type="text"
+        />
+        <div className="col-wrapper">
+          <FiClock size={20} color="var(--text)" />
+        </div>
+      </div>
+      {error && showError && (
+        <span className="error">{error}</span>
+      )}
+    </div>
+  );
+}
