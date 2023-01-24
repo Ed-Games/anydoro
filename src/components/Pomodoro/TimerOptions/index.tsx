98b989e9baf76a26d76d8c9becec82abe77d437e
Ed-Games <edmarcosfilho2015@gmail.com>
2023-01-24 09:49:36 -0300

    update: wrappped normal form with Formik and replaced normal inputs with the time input component

--- src/components/Pomodoro/TimerOptions/index.tsx
+++ src/components/Pomodoro/TimerOptions/index.tsx
@@ -1,7 +1,10 @@
+import { Formik } from "formik";
 import { Modal } from "../../Modal";
 import styles from "./styles.module.scss";
+import { timerOptionsSchema } from "../../../validators/timerOptionsSchema";
+import { TimeInput } from "../../TimeInput";
 
-interface ITImerOptionsProps {
+interface ITimerOptionsProps {
   isVisible: boolean;
   setIsVisible: (arg: boolean) => void;
 }
@@ -9,32 +12,53 @@ interface ITImerOptionsProps {
 export const TimerOptions = ({
   isVisible,
   setIsVisible,
-}: ITImerOptionsProps) => {
+}: ITimerOptionsProps) => {
   return (
     <Modal isVisible={isVisible} setIsModalVisible={setIsVisible}>
-      <form className={styles.content}>
-        <h3>Configurar Timer</h3>
-        <div className={`row-wrapper ${styles.InputContainer}`}>
-          <div className="column-wrapper">
-            <span>Pomodoro</span>
-            <input type="time" />
-          </div>
-          <div className="column-wrapper">
-            <span>Short Break</span>
-            <input type="time" />
-          </div>
-          <div className="column-wrapper">
-            <span>Long Break</span>
-            <input type="time" />
-          </div>
-        </div>
+      <Formik
+        initialValues={{ pomodoro: "", shortBreak: "", longBreak: "" }}
+        validationSchema={timerOptionsSchema}
+        onSubmit={(values) => console.log(values)}
+      >
+        {({ handleChange, handleSubmit, values, errors, touched }) => (
+          <form onSubmit={handleSubmit} className={styles.content}>
+            <h3>Configurar Timer</h3>
+            <div className={`row-wrapper ${styles.InputContainer}`}>
+              <TimeInput
+                title="Pomodoro"
+                name="pomodoro"
+                value={values.pomodoro}
+                error={errors.pomodoro}
+                showError={touched.pomodoro}
+                onChange={handleChange}
+              />
+              <TimeInput
+                title="Short Break"
+                name="shortBreak"
+                value={values.shortBreak}
+                error={errors.shortBreak}
+                showError={touched.shortBreak}
+                onChange={handleChange}
+              />
+              <TimeInput
+                title="Long Break"
+                name="longBreak"
+                value={values.longBreak}
+                error={errors.longBreak}
+                showError={touched.longBreak}
+                onChange={handleChange}
+              />
+            </div>
 
-        <div className={`row-wrapper ${styles.btnContainer}`}>
-          <button>Cancelar</button>
-          <button>Salvar</button>
-        </div>
-
-      </form>
+            <div className={`row-wrapper ${styles.btnContainer}`}>
+              <button type="button" onClick={() => setIsVisible(false)}>
+                Cancelar
+              </button>
+              <button type="submit">Salvar</button>
+            </div>
+          </form>
+        )}
+      </Formik>
     </Modal>
   );
 };
