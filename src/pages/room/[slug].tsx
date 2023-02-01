9249952a75b0366a503a8d6f9095ee1c79764ced
Ed-Games <edmarcosfilho2015@gmail.com>
2023-02-01 10:44:43 -0300

    fix: added missed styles import for toastify notifications

--- src/pages/room/[slug].tsx
+++ src/pages/room/[slug].tsx
@@ -4,6 +4,7 @@ import { ToastContainer } from "react-toastify";
 import Header from "../../components/Header";
 import Pomodoro from "../../components/Pomodoro";
 import { TimerContextProvider } from "../../contexts/timerContext";
+import 'react-toastify/dist/ReactToastify.css';
 
 const Room: NextPage = () => {
   return (
