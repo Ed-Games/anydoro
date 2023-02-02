a2fe8a304589d4d6bdead3db53e67bdf3fc2879d
Ed-Games <edmarcosfilho2015@gmail.com>
2023-02-02 08:21:45 -0300

    refactor: created presentation component

---
+++ src/components/presentation/index.tsx
@@ -0,0 +1,10 @@
+import styles from './styles.module.scss';
+
+export const Presentation = () => {
+  return (
+    <aside className={styles.presentation}>
+      <h1>Crie salas de colaboração em tempo real</h1>
+      <span>Aumente sua produtividade com trabalho em grupo</span>
+    </aside>
+  );
+};
