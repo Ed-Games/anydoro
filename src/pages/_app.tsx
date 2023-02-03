8066c213d388fb9fa4880316117a4089d50dd595
Ed-Games <edmarcosfilho2015@gmail.com>
2023-02-03 11:40:29 -0300

    fix: moved ToastContainer to the app to allow notifications outside the room

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
 import { AuthContextProvider } from "../contexts/AuthContext";
+import { ToastContainer } from "react-toastify";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <AuthContextProvider>
       <Component {...pageProps} />
+      <ToastContainer />
     </AuthContextProvider>
   );
 }
