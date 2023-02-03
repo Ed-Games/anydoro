c6efa264c12d188b403e089f28b06eb09c248d92
Ed-Games <edmarcosfilho2015@gmail.com>
2023-02-03 11:41:14 -0300

    update: verify if room has endend to redirect the user to create or join another room

--- src/pages/rooms/[slug].tsx
+++ src/pages/rooms/[slug].tsx
@@ -1,12 +1,31 @@
 import { NextPage } from "next";
 import Head from "next/head";
-import { ToastContainer } from "react-toastify";
+import { toast } from "react-toastify";
 import Header from "../../components/Header";
 import Pomodoro from "../../components/Pomodoro";
 import { TimerContextProvider } from "../../contexts/timerContext";
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
+import { useEffect } from "react";
+import { onValue, ref } from "firebase/database";
+import { database } from "../../services/firebase";
+import { useRouter } from "next/router";
 
 const Room: NextPage = () => {
+  const router = useRouter();
+  const { slug } = router.query;
+
+  useEffect(() => {
+    const roomRef = ref(database, `rooms/${slug}`);
+    onValue(roomRef, (snapshot) => {
+      const room = snapshot.val();
+
+      if (room && room.endedAt) {
+        toast.warn("Essa sala foi encerrada.");
+        router.push("/");
+      }
+    });
+  }, [router, slug]);
+
   return (
     <TimerContextProvider>
       <div className="container">
@@ -16,7 +35,6 @@ const Room: NextPage = () => {
         <Header />
         <Pomodoro />
       </div>
-      <ToastContainer />
     </TimerContextProvider>
   );
 };
