1bbc2ad43ddebedf249e4346d3540b16b356075d
Ed-Games <edmarcosfilho2015@gmail.com>
2023-02-03 11:06:26 -0300

    update: added schema validation to form and calling the `handleCreateRoom` when submited to create a room in the firebase backend

--- src/pages/rooms/new/index.tsx
+++ src/pages/rooms/new/index.tsx
@@ -5,8 +5,27 @@ import logo from "../../../../public/logo.svg";
 import Image from "next/image";
 import { Formik } from "formik";
 import Link from "next/link";
+import { CreateRoomSchema } from "../../../validators/createRoomSchema";
+import { database } from "../../../services/firebase";
+import { ref, set } from "firebase/database";
+import { uuidv4 } from "@firebase/util";
+import { useAuth } from "../../../hooks/useAuth";
+import Router from "next/router";
+import { toast } from "react-toastify";
 
 const NewRoom: NextPage = () => {
+  const { user } = useAuth();
+
+  const handleCreateRoom = async (name: string) => {
+    try {
+      const roomRef = ref(database, "rooms/" + uuidv4());
+      await set(roomRef, { name, adminId: user!.id });
+      Router.push(`/rooms/${roomRef.key}`);
+    } catch (error) {
+      toast.error("Houve um erro ao tentar criar a sala.");
+    }
+  };
+
   return (
     <div className={styles.container}>
       <Presentation />
@@ -15,10 +34,20 @@ const NewRoom: NextPage = () => {
           <Image src={logo} alt="Anydoro logo" />
         </div>
         <h2>Criar uma nova sala</h2>
-        <Formik initialValues={{}} onSubmit={(values) => console.log(values)}>
-          {({ handleChange, handleSubmit, values, errors, touched }) => (
+        <Formik
+          initialValues={{ name: "" }}
+          validationSchema={CreateRoomSchema}
+          onSubmit={(values) => handleCreateRoom(values.name)}
+        >
+          {({ handleChange, handleSubmit, errors }) => (
             <form onSubmit={handleSubmit}>
-              <input placeholder="Nome da sala" aria-label="nome da sala" />
+              <input
+                name="name"
+                onChange={handleChange}
+                placeholder="Nome da sala"
+                aria-label="nome da sala"
+              />
+              {errors.name && <span className="error">{errors.name}</span>}
               <button type="submit">Criar sala</button>
               <span>
                 Quer entrar numa sala existente?{" "}
