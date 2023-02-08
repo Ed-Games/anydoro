bb0b74c939b24350a9473c5c434f0fd1d34971eb
Ed-Games <edmarcosfilho2015@gmail.com>
2023-02-08 09:44:51 -0300

    update: added component around the main component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,15 @@ import "../styles/globals.scss";
 import type { AppProps } from "next/app";
 import { AuthContextProvider } from "../contexts/AuthContext";
 import { ToastContainer } from "react-toastify";
+import { RoomContextProvider } from "../contexts/RoomContext";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <AuthContextProvider>
-      <Component {...pageProps} />
-      <ToastContainer />
+      <RoomContextProvider>
+        <Component {...pageProps} />
+        <ToastContainer />
+      </RoomContextProvider>
     </AuthContextProvider>
   );
 }
