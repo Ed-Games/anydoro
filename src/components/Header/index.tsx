5bf48b88b37f271c87139c22fef470968437cf9b
Ed-Games <edmarcosfilho2015@gmail.com>
2023-02-08 11:23:09 -0300

    update: added `copyRoomCodeToClipBoard` method  and triggering it on click in the share button

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,19 @@
 import { NextPage } from "next";
+import { useRouter } from "next/router";
+import { toast } from "react-toastify";
 import styles from './styles.module.scss';
 
 const Header: NextPage = () => {
+  const router = useRouter();
+
+  const copyRoomCodeToClipBoard = () => {
+    navigator.clipboard.writeText(router.query.slug as string);
+    toast.success('Código da sala copiado com sucesso!');
+  }
+
   return (
     <div className={styles.header}>
-      <button>Compartilhar</button>
+      <button onClick={copyRoomCodeToClipBoard}>Compartilhar</button>
       <button>Encerrar sala</button>
     </div>
   )
