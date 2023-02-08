82c8c8c87d045126c80c7a5c57589898c363f1b4
Ed-Games <edmarcosfilho2015@gmail.com>
2023-02-08 09:46:47 -0300

    update: removed the `handleLoadRoomAndAddUser` method  abd its dependencies and using the one from the context

--- src/pages/rooms/[slug].tsx
+++ src/pages/rooms/[slug].tsx
@@ -1,60 +1,25 @@
 import { NextPage } from "next";
 import Head from "next/head";
-import { toast } from "react-toastify";
 import Header from "../../components/Header";
 import Pomodoro from "../../components/Pomodoro";
 import { TimerContextProvider } from "../../contexts/timerContext";
 import "react-toastify/dist/ReactToastify.css";
-import { memo, useCallback, useEffect, useState } from "react";
-import { onValue, ref, set } from "firebase/database";
-import { database } from "../../services/firebase";
+import { memo, useEffect } from "react";
 import { useRouter } from "next/router";
 import { useAuth } from "../../hooks/useAuth";
-import { IRoom } from "../../interfaces/Room";
-import { IUser } from "../../interfaces/User";
+import { useRoom } from "../../hooks/useRoom";
 
 const RoomComponent: NextPage = () => {
-  const [room, setRoom] = useState<IRoom>();
   const { user } = useAuth();
   const router = useRouter();
-
-  const handleLoadRoomAndAddUser = useCallback(() => {
-    const roomRef = ref(database, `rooms/${router.query.slug}`);
-    onValue(roomRef, async (snapshot) => {
-      const localRoom: IRoom = snapshot.val();
-      if (localRoom) {
-        if (localRoom.endedAt) {
-          toast.warn("Essa sala foi encerrada.");
-          router.push("/");
-        }
-
-        const usersInRoom = localRoom.users
-          ? Array.from(localRoom.users)
-          : new Array<IUser>();
-
-        const alreadyexists = usersInRoom.find(
-          (userInRoom) => userInRoom.id === user!.id
-        );
-
-        if (!alreadyexists) {
-          const userRef = ref(
-            database,
-            `rooms/${router.query.slug}/users/${user!.id}`
-          );
-          await set(userRef, {
-            ...user,
-            isAdmin: localRoom.adminId === user!.id,
-          });
-        }
-      }
-
-      setRoom(localRoom);
-    });
-  }, [router, user]);
+  const { handleLoadRoomAndAddUser } = useRoom();
 
   useEffect(() => {
-    !user && router.push("/");
-    handleLoadRoomAndAddUser();
+    if (!user) {
+      router.push("/");
+      return;
+    }
+    handleLoadRoomAndAddUser(user);
   }, [handleLoadRoomAndAddUser, router, user]);
 
   return (
