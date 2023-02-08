f8ceba8323fea2f7dae09a1e7ba960969289f999
Ed-Games <edmarcosfilho2015@gmail.com>
2023-02-07 22:58:13 -0300

    update: created function to load the room to state and add the user to it.

--- src/pages/rooms/[slug].tsx
+++ src/pages/rooms/[slug].tsx
@@ -5,26 +5,57 @@ import Header from "../../components/Header";
 import Pomodoro from "../../components/Pomodoro";
 import { TimerContextProvider } from "../../contexts/timerContext";
 import "react-toastify/dist/ReactToastify.css";
-import { useEffect } from "react";
-import { onValue, ref } from "firebase/database";
+import { memo, useCallback, useEffect, useState } from "react";
+import { onValue, ref, set } from "firebase/database";
 import { database } from "../../services/firebase";
 import { useRouter } from "next/router";
+import { useAuth } from "../../hooks/useAuth";
+import { IRoom } from "../../interfaces/Room";
+import { IUser } from "../../interfaces/User";
 
-const Room: NextPage = () => {
+const RoomComponent: NextPage = () => {
+  const [room, setRoom] = useState<IRoom>();
+  const { user } = useAuth();
   const router = useRouter();
-  const { slug } = router.query;
+
+  const handleLoadRoomAndAddUser = useCallback(() => {
+    const roomRef = ref(database, `rooms/${router.query.slug}`);
+    onValue(roomRef, async (snapshot) => {
+      const localRoom: IRoom = snapshot.val();
+      if (localRoom) {
+        if (localRoom.endedAt) {
+          toast.warn("Essa sala foi encerrada.");
+          router.push("/");
+        }
+
+        const usersInRoom = localRoom.users
+          ? Array.from(localRoom.users)
+          : new Array<IUser>();
+
+        const alreadyexists = usersInRoom.find(
+          (userInRoom) => userInRoom.id === user!.id
+        );
+
+        if (!alreadyexists) {
+          const userRef = ref(
+            database,
+            `rooms/${router.query.slug}/users/${user!.id}`
+          );
+          await set(userRef, {
+            ...user,
+            isAdmin: localRoom.adminId === user!.id,
+          });
+        }
+      }
+
+      setRoom(localRoom);
+    });
+  }, [router, user]);
 
   useEffect(() => {
-    const roomRef = ref(database, `rooms/${slug}`);
-    onValue(roomRef, (snapshot) => {
-      const room = snapshot.val();
-
-      if (room && room.endedAt) {
-        toast.warn("Essa sala foi encerrada.");
-        router.push("/");
-      }
-    });
-  }, [router, slug]);
+    !user && router.push("/");
+    handleLoadRoomAndAddUser();
+  }, [handleLoadRoomAndAddUser, router, user]);
 
   return (
     <TimerContextProvider>
@@ -38,5 +69,7 @@ const Room: NextPage = () => {
     </TimerContextProvider>
   );
 };
+
+const Room = memo(RoomComponent);
 
 export default Room;
