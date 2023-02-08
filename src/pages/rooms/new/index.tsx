626a9101ee4dea4e26202b856f5653dc8adcc645
Ed-Games <edmarcosfilho2015@gmail.com>
2023-02-07 22:56:46 -0300

    update: saving room with createdAt field

--- src/pages/rooms/new/index.tsx
+++ src/pages/rooms/new/index.tsx
@@ -19,7 +19,7 @@ const NewRoom: NextPage = () => {
   const handleCreateRoom = async (name: string) => {
     try {
       const roomRef = ref(database, "rooms/" + uuidv4());
-      await set(roomRef, { name, adminId: user!.id });
+      await set(roomRef, { name, adminId: user!.id, createdAt: new Date() });
       Router.push(`/rooms/${roomRef.key}`);
     } catch (error) {
       toast.error("Houve um erro ao tentar criar a sala.");
