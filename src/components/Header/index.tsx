e1b0d13b5433191576550964c97242d9e3f3bf72
Ed-Games <edmarcosfilho2015@gmail.com>
2023-02-11 08:46:37 -0300

    update: added CloseRoom, directly to the room element using createPortal,   and using state to control when it should be visible

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,22 +1,36 @@
 import { NextPage } from "next";
 import { useRouter } from "next/router";
+import { useState } from "react";
+import { createPortal } from "react-dom";
 import { toast } from "react-toastify";
-import styles from './styles.module.scss';
+import { CloseRoom } from "../CloseRoom";
+import styles from "./styles.module.scss";
 
 const Header: NextPage = () => {
+  const [isCloseModalVisible, setIsCloseModalVisible] =
+    useState<boolean>(false);
   const router = useRouter();
 
   const copyRoomCodeToClipBoard = () => {
     navigator.clipboard.writeText(router.query.slug as string);
-    toast.success('Código da sala copiado com sucesso!');
-  }
+    toast.success("Código da sala copiado com sucesso!");
+  };
 
   return (
     <div className={styles.header}>
       <button onClick={copyRoomCodeToClipBoard}>Compartilhar</button>
-      <button>Encerrar sala</button>
+      <button onClick={() => setIsCloseModalVisible(true)}>
+        Encerrar sala
+      </button>
+      { isCloseModalVisible && createPortal(
+        <CloseRoom
+          isVisible={isCloseModalVisible}
+          setIsVisible={setIsCloseModalVisible}
+        />,
+        document.getElementById('room')!
+      )}
     </div>
-  )
-}
+  );
+};
 
 export default Header;
