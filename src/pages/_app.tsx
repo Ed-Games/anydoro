ae01cd47ac400c0d60e74703d6cb6dd915583470
Ed-Games <edmarcosfilho2015@gmail.com>
2023-02-11 17:34:58 -0300

    update: added meta tag for viewport configuration and link to manifest.json

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,11 +3,19 @@ import type { AppProps } from "next/app";
 import { AuthContextProvider } from "../contexts/AuthContext";
 import { ToastContainer } from "react-toastify";
 import { RoomContextProvider } from "../contexts/RoomContext";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <AuthContextProvider>
       <RoomContextProvider>
+        <Head>
+          <meta
+            name="viewport"
+            content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+          />
+          <link rel="manifest" href="/manifest.json" />
+        </Head>
         <Component {...pageProps} />
         <ToastContainer />
       </RoomContextProvider>
