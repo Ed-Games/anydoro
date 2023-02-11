e6503642766335fc495dbad7ecb0575759da87f2
Ed-Games <edmarcosfilho2015@gmail.com>
2023-02-11 17:33:17 -0300

    update: adicionada configuração para o pwa e habilitando modo offline

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+const withPWA = require("next-pwa")({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  register: true,
+  sw: "/service-worker.js",
+});
+const withOffline = require("next-offline");
+
+const nextConfig = withPWA({
   reactStrictMode: true,
   swcMinify: true,
-}
+});
 
-module.exports = nextConfig
+module.exports = withOffline(nextConfig);
