efc6a8f02e9a5f8b041b3fa17a037d30fa9bc622
Ed-Games <edmarcosfilho2015@gmail.com>
2023-02-15 09:00:01 -0300

    refactor: code reformated

--- src/components/TimerOptions/index.tsx
+++ src/components/TimerOptions/index.tsx
@@ -42,64 +42,64 @@ export const TimerOptions = ({
     setIsVisible(false);
   };
 
-  useEffect(()=> {
-    if(timerOptions){
+  useEffect(() => {
+    if (timerOptions) {
       setInitialValues({
         longBreak: secondsToText(timerOptions.longBreak),
         pomodoro: secondsToText(timerOptions.pomodoro),
-        shortBreak: secondsToText(timerOptions.shortBreak)
-      })
-    } 
-  }, [timerOptions])
+        shortBreak: secondsToText(timerOptions.shortBreak),
+      });
+    }
+  }, [timerOptions]);
 
   return (
     <Modal isVisible={isVisible} setIsModalVisible={setIsVisible}>
-      { initialValues && (
+      {initialValues && (
         <Formik
-        initialValues={initialValues}
-        validationSchema={timerOptionsSchema}
-        onSubmit={(values) => handleSavePomodoroOptions(values)}
-      >
-        {({ handleChange, handleSubmit, values, errors, touched }) => (
-          <form onSubmit={handleSubmit} className={styles.content}>
-            <h3>Configurar Timer</h3>
-            <div className={`row-wrapper ${styles.InputContainer}`}>
-              <TimeInput
-                title="Pomodoro"
-                name="pomodoro"
-                value={values.pomodoro}
-                error={errors.pomodoro}
-                showError={touched.pomodoro}
-                onChange={handleChange}
-              />
-              <TimeInput
-                title="Short Break"
-                name="shortBreak"
-                value={values.shortBreak}
-                error={errors.shortBreak}
-                showError={touched.shortBreak}
-                onChange={handleChange}
-              />
-              <TimeInput
-                title="Long Break"
-                name="longBreak"
-                value={values.longBreak}
-                error={errors.longBreak}
-                showError={touched.longBreak}
-                onChange={handleChange}
-              />
-            </div>
+          initialValues={initialValues}
+          validationSchema={timerOptionsSchema}
+          onSubmit={(values) => handleSavePomodoroOptions(values)}
+        >
+          {({ handleChange, handleSubmit, values, errors, touched }) => (
+            <form onSubmit={handleSubmit} className={styles.content}>
+              <h3>Configurar Timer</h3>
+              <div className={`row-wrapper ${styles.InputContainer}`}>
+                <TimeInput
+                  title="Pomodoro"
+                  name="pomodoro"
+                  value={values.pomodoro}
+                  error={errors.pomodoro}
+                  showError={touched.pomodoro}
+                  onChange={handleChange}
+                />
+                <TimeInput
+                  title="Short Break"
+                  name="shortBreak"
+                  value={values.shortBreak}
+                  error={errors.shortBreak}
+                  showError={touched.shortBreak}
+                  onChange={handleChange}
+                />
+                <TimeInput
+                  title="Long Break"
+                  name="longBreak"
+                  value={values.longBreak}
+                  error={errors.longBreak}
+                  showError={touched.longBreak}
+                  onChange={handleChange}
+                />
+              </div>
 
-            <div className={`row-wrapper ${styles.btnContainer}`}>
-              <button type="button" onClick={() => setIsVisible(false)}>
-                Cancelar
-              </button>
-              <button type="submit">Salvar</button>
-            </div>
-          </form>
-        )}
-      </Formik>
-      ) }
+              <div className={`row-wrapper ${styles.btnContainer}`}>
+                <button type="button" onClick={() => setIsVisible(false)}>
+                  Cancelar
+                </button>
+                <button type="submit">Salvar</button>
+              </div>
+            </form>
+          )}
+        </Formik>
+      )}
     </Modal>
   );
 };
