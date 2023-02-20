c8917ac37b1e64d7b40e4c0e9b68b9e3586a1880
Ed-Games <edmarcosfilho2015@gmail.com>
2023-02-20 09:56:14 -0300

    update: removed unused dependencies

--- src/components/Pomodoro/index.tsx
+++ src/components/Pomodoro/index.tsx
@@ -6,7 +6,6 @@ import { TimerOptions } from "../TimerOptions";
 import { Progressbar } from "../ProgressBar";
 import { Mode } from "../../enums";
 import { toast } from "react-toastify";
-import { useRoom } from "../../hooks/useRoom";
 
 const Pomodoro: NextPage = () => {
   const [openConfigModal, setOpenConfigModal] = useState<boolean>(false);
@@ -14,15 +13,12 @@ const Pomodoro: NextPage = () => {
   const {
     minutes,
     seconds,
-    time,
     startTimer,
     resetTimer,
     isActive,
     mode,
     cyclesCount,
   } = useTimer();
-
-  const { handleSetRoomTimer } = useRoom();
 
   const [minuteLeft, minuteRight] = String(minutes).padStart(2, "0").split("");
   const [secondLeft, secondRight] = String(seconds).padStart(2, "0").split("");
